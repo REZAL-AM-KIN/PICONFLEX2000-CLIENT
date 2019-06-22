203de339817cd1c43e6cee599b0a2bf884e0e2de
REZALKIN <info.kin@gadz.org>
2019-06-22 16:39:21 +0200

    bla

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ exec(open('/home/pi/PICONFLEX2000-CLIENT/MFRC522.py').read()) #Execute le script
 print("Importation Fonctions")
 for root, dirs, files in os.walk("/home/pi/PICONFLEX2000-FONCTIONS"): #Scan toute les scripts du dossier des fonctions du système
         for names in files:
-            print(names)
             if names[-3:]==".py": #Evite l'execution du dossier .git
+                print(names)
                 exec(open(os.path.join(root, names)).read()) #Execute toute ces scripts pour définir les fonctions dans la console actuelle
